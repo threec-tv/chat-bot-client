a8d53f9e19803bcd95cf2dca980dfb319643eba6
Adam Meadows <adamlmeadows@googlemail.com>
2022-06-16 14:45:53 +0100

    Tests

---
+++ java/src/test/java/tv/threec/chatbotclient/java/ApplicationTests.java
@@ -0,0 +1,15 @@
+package tv.threec.chatbotclient.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
